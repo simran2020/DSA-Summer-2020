37fb0ba0c0b92f870fdf8d7f9e1e75ca9d1005ba
simran2020 <62429410+simran2020@users.noreply.github.com>
2020-09-30 12:30:41 -0700

    Update Demo 2.py

--- Demo 2.py
+++ Demo 2.py
@@ -1,5 +1,6 @@
-def Greetings():
-    name = input("Hi!, what's your name? :")
+def Greetings(name):
     print("Hello there" ,name, ", my name is Jason and i am taking Data Structures with you.")
     print("Nice to meet you.")
-Greetings()
+
+name = input("Hi!, what's your name? :")    
+Greetings(name)
